f72f3eabe4ac46c0b5dd4a364c49bdc525464d29
Viacheslav Lozinskyi <viacheslav.lozinskyi@gmail.com>
2023-04-30 09:55:43 +0300

    - Used NuGet packages are updated

--- application/preview-cpp.vs/resource/package/VSPackage.cs
+++ application/preview-cpp.vs/resource/package/VSPackage.cs
@@ -28,6 +28,7 @@ namespace resource.package
             public const string COPYRIGHT = "Copyright (c) 2020 by Nesviatypaskha Oleksii. All rights reserved.";
             public const string DESCRIPTION = "Quick preview of C, CPP, H and HPP files";
             public const string GUID = "42533993-FCE3-42E5-85C5-F339A242EF10";
+            public const string HOST = "MetaOutput";
             public const string NAME = "Preview-CPP";
             public const string VERSION = "0.8.7";
         }
@@ -54,7 +55,7 @@ namespace resource.package
                         var a_Context1 = (OutputWindowPane)null;
                         for (var i = a_Context.ToolWindows.OutputWindow.OutputWindowPanes.Count; i >= 1; i--)
                         {
-                            if (a_Context.ToolWindows.OutputWindow.OutputWindowPanes.Item(i).Name == "MetaOutput")
+                            if (a_Context.ToolWindows.OutputWindow.OutputWindowPanes.Item(i).Name == CONSTANT.HOST)
                             {
                                 a_Context1 = a_Context.ToolWindows.OutputWindow.OutputWindowPanes.Item(i);
                                 break;
@@ -62,7 +63,7 @@ namespace resource.package
                         }
                         if (a_Context1 == null)
                         {
-                            a_Context1 = a_Context.ToolWindows.OutputWindow.OutputWindowPanes.Add("MetaOutput");
+                            a_Context1 = a_Context.ToolWindows.OutputWindow.OutputWindowPanes.Add(CONSTANT.HOST);
                         }
                         if (a_Context1 != null)
                         {
